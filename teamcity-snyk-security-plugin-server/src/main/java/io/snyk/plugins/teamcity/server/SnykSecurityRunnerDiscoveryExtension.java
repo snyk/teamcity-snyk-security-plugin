be39f367520acbf8a9a0a3149f930dff516691a9
pavel <36902936+pavel-github@users.noreply.github.com>
2019-04-04 13:04:28 +0200

    Add auto-detect feature for snyk security build step

---
+++ teamcity-snyk-security-plugin-server/src/main/java/io/snyk/plugins/teamcity/server/SnykSecurityRunnerDiscoveryExtension.java
@@ -0,0 +1,51 @@
+package io.snyk.plugins.teamcity.server;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import jetbrains.buildServer.serverSide.discovery.BreadthFirstRunnerDiscoveryExtension;
+import jetbrains.buildServer.serverSide.discovery.DiscoveredObject;
+import jetbrains.buildServer.util.CollectionsUtil;
+import jetbrains.buildServer.util.browser.Element;
+import org.jetbrains.annotations.NotNull;
+
+import static io.snyk.plugins.teamcity.common.SnykSecurityRunnerConstants.MONITOR_PROJECT_ON_BUILD;
+import static io.snyk.plugins.teamcity.common.SnykSecurityRunnerConstants.RUNNER_TYPE;
+import static io.snyk.plugins.teamcity.common.SnykSecurityRunnerConstants.SEVERITY_THRESHOLD;
+
+public class SnykSecurityRunnerDiscoveryExtension extends BreadthFirstRunnerDiscoveryExtension {
+
+  private static final Set<String> SUPPORTED_FILES = CollectionsUtil.setOf("yarn.lock",
+                                                                           "package-lock.json",
+                                                                           "package.json",
+                                                                           "Gemfile.lock",
+                                                                           "pom.xml",
+                                                                           "build.gradle",
+                                                                           "build.sbt",
+                                                                           "Pipfile",
+                                                                           "requirements.txt",
+                                                                           "Gopkg.lock",
+                                                                           "vendor/vendor.json",
+                                                                           "obj/project.assets.json",
+                                                                           "packages.config",
+                                                                           "composer.lock",
+                                                                           "build.gradle.kts");
+
+  @NotNull
+  @Override
+  protected List<DiscoveredObject> discoverRunnersInDirectory(@NotNull Element dir, @NotNull List<Element> filesAndDirs) {
+    List<DiscoveredObject> runners = new ArrayList<>(1);
+    boolean enableSnykSecurityBuildStep = filesAndDirs.stream().anyMatch(element -> element.isLeaf() && SUPPORTED_FILES.contains(element.getName()));
+
+    if (enableSnykSecurityBuildStep) {
+      Map<String, String> defaultProperties = new HashMap<>(2);
+      defaultProperties.put(SEVERITY_THRESHOLD, "low");
+      defaultProperties.put(MONITOR_PROJECT_ON_BUILD, "true");
+      runners.add(new DiscoveredObject(RUNNER_TYPE, defaultProperties));
+    }
+    return runners;
+  }
+}
