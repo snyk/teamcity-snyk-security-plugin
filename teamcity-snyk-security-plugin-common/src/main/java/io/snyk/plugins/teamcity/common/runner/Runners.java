5e450aa0ecc8ae93f27c21940a02280e0b4e4fdf
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2020-06-25 14:09:02 +0200

    feat: update CLI version to 1.348.2

--- teamcity-snyk-security-plugin-common/src/main/java/io/snyk/plugins/teamcity/common/runner/Runners.java
+++ teamcity-snyk-security-plugin-common/src/main/java/io/snyk/plugins/teamcity/common/runner/Runners.java
@@ -12,7 +12,7 @@ import static java.util.Arrays.asList;
 public final class Runners {
 
   private static final TreeMap<String, RunnerVersion> AVAILABLE_RUNNERS = new TreeMap<>();
-  private static final String DEFAULT_VERSION = "1.258.1";
+  private static final String DEFAULT_VERSION = "1.348.2";
 
   // all bundled versions should be initialized here
   static {
